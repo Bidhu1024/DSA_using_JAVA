56591ef73fe9828e417ae3c96819ed8f033d384e
Bidhubhushan Gahan <89645530+Bidhu1024@users.noreply.github.com>
2022-05-24 07:21:49 +0530

    Update Span.java

--- Span.java
+++ Span.java
@@ -1,4 +1,13 @@
+Span Of Array
 
+
+1. You are given a number n, representing the count of elements.
+2. You are given n numbers.
+3. You are required to find the span of input. Span is defined as difference of maximum value and minimum value. 
+    
+    
+    
+    
 import java.util.*;
 
 public class Span{
